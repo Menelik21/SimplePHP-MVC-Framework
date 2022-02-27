8a14e30e102cf8454968a2f31110fd364326b40b
Menelik Belete <65121095+Menelik21@users.noreply.github.com>
2022-02-27 16:03:32 +0300

    Update session_helper.php

--- app/helper/session_helper.php
+++ app/helper/session_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This session helper used to display flash message like info, warning, alert and so on.
- * And also the app us this helper session_start to set $_SESSION on various page
+ * And also the app us this helper session_start to set $_SESSION on various page using the below session method
  */
 session_start();
 
@@ -15,7 +15,7 @@ function flashMessageCreate(string $name, string $message): void
     $_SESSION[$name] = $message;
 }
 
-function flashMessageDisplay(string $name): void
+function flashMessageDisplay(string $name)
 {
     if (!isset($_SESSION[$name])) {
         return;
@@ -23,21 +23,42 @@ function flashMessageDisplay(string $name): void
     // Assigne session text to flash message and unset the session
     $flash_message = $_SESSION[$name];
     unset($_SESSION[$name]);
-    echo ($flash_message);
+    return ($flash_message);
 }
 
-function flash(string $name = '', string $message =''): void
+function flash(string $name = '', string $message ='')
 {
     if ($name !== '' && $message !== '') {
         // create a flash message
         flashMessageCreate($name, $message);
     } elseif ($name !== '' && $message === '') {
         // display a flash message
-        flashMessageDisplay($name);
+        return flashMessageDisplay($name);
     }
     else{
       return;
     }
+}
+/**
+ * This session helper method used to create a sssion on various place of the application.
+ * Eg. session("loginerror","Incorrect username or password");
+ * To display the message just call the session method session("loginError")
+ */
+function session(string $sessionName = '', string $sessionValue = ''){
+
+    if (!empty($sessionName) & !empty($sessionValue)) {
+        #create the session
+        $_SESSION[$sessionName] = $sessionValue;
+        return $_SESSION[$sessionName];
+    }
+    elseif (!empty($sessionName) && empty($sessionValue) && isset($_SESSION[$sessionName])) {
+        #here we return the session with its name
+        return $_SESSION[$sessionName];
+    }
+    else{
+        return;
+    }
+    
 }
 
 ?>
