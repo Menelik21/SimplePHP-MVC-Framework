0b193e3d1f23f8d0e5578832b4674095508d771f
Menelik Belete <65121095+Menelik21@users.noreply.github.com>
2022-02-28 09:47:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once "app/core/Core.php";
 require_once "app/config/config.php";
 require_once "app/config/database.php";
 require_once "app/helper/session_helper.php";
+require_once "app/helper/form_helper.php";
 require_once "app/helper/url_helper.php";
 
 $appCore = new Core;
